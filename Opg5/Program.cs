1269daf48bfc6ed72a343f707ee962cafb26fcf3
janus <japa@lb.dk>
2017-05-02 16:13:53 +0200

    Opg05 Strings

---
+++ Opg5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String ForNavn = "Mikkel";
+            String EfterNavn = "Cronberg";
+            Console.WriteLine(String.Concat(ForNavn," " , EfterNavn));
+            Console.ReadKey();
+
+            String FuldtNavn = (ForNavn +" "+EfterNavn);
+            String Stort = FuldtNavn.ToUpper();
+            String Lille = FuldtNavn.ToLower();
+            Console.WriteLine(Stort);
+            Console.ReadKey();
+
+            String del = FuldtNavn.Substring(7, 4);
+            Console.WriteLine(del);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}

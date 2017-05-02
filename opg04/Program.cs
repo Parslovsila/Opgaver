dce4036ee04dd186f46413f546f3ac8934532d46
janus <japa@lb.dk>
2017-05-02 14:32:48 +0200

    OPG 4 array

---
+++ opg04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] løn ={95000, 20000, 30000, 40000, 23512};
+            Double total = 0;
+
+            Array.Sort(løn);
+
+            for (int i = 0; i < løn.Length; i++)
+            {
+                total +=løn[i];
+                Console.WriteLine(løn[i]);
+                }
+
+            Double gennemsnit = total / løn.Length;
+
+            Console.WriteLine(gennemsnit.ToString("C2"));
+            Console.ReadKey();
+
+        }
+        }
+    }
+

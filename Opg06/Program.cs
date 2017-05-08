6b59a5d4820a73604872137b602ef2965f3ca578
Janus Parslov <japa@lb.dk>
2017-05-08 11:56:13 +0200

    Insat tabel med farver

---
+++ Opg06/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         for (int i = 1; i < 11; i++)
+          {
+           for (int x = 1; x < 11; x++)
+            {
+             int resultat = i * x; if ((i * x) > 50)
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+             Console.Write(resultat.ToString().PadLeft(4));
+            }
+             Console.WriteLine();
+             Console.ReadKey();
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}

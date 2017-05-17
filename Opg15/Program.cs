a4531b06698071041a498476c36cd8a0b9ccbfe3
Janus <japa@lb.dk>
2017-05-17 13:58:20 +0200

    nye opgaver

---
+++ Opg15/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg15
+{
+    public class person
+    {
+     public string Fornavn
+        {
+            get; set;
+        }
+        public string Efternavn
+        {
+            get; set;
+        }
+        public string Fuldtnavn()
+        {
+            return $"{Fornavn} {Efternavn}";
+        }
+    }
+    public class Elev : person
+    {
+     public string KlasseLokale
+        {
+            get; set;
+        }
+        
+    }
+    public class Instruktør : person
+    {
+        public int NøgleId
+        {
+            get; set;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person p = new person()
+            {
+                Efternavn = "x",
+                Fornavn = "y"
+            };
+            Elev e = new Elev()
+            {
+                Efternavn = "t",
+                Fornavn = "g",
+                KlasseLokale = "a"
+            };
+            Instruktør i = new Instruktør()
+            {
+                Efternavn = "2",
+                Fornavn = "s",
+                NøgleId = 1
+            };
+
+            person p1 =new person { Efternavn = "Parslov", Fornavn = "Janus" };
+            
+
+            
+            Console.WriteLine("Person: " + p.Fuldtnavn());
+            Console.WriteLine("Ny Person: " + p1.Fuldtnavn());
+            Console.WriteLine("Elev: " +e.Fuldtnavn());
+            Console.WriteLine("instruktør: "+i.Fuldtnavn());
+            Console.ReadKey();
+            
+    }
+    }
+}
---
+++ Opg16/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg16
+{
+
+    public class Terning
+    {
+     private static System.Random rnd;
+     private int værdi;
+     public int Værdi
+        {
+         get
+            {
+                Console.WriteLine("Terning aflæses som " + this.værdi);
+                return this.værdi;
+            }
+         set
+            {
+
+                if (value < 0 || value > 6)
+                    value = 1;
+
+                Console.WriteLine("Terning tildeles " + value);
+                this.værdi = value;
+            }
+        }
+
+        public void Skriv()
+        {
+            Console.WriteLine("[" + this.Værdi + "]");
+        }
+        public void Ryst()
+        {
+             this.Værdi = rnd.Next(1, 7);
+        }
+        public Terning()
+        {
+            this.Ryst();
+        }
+        public Terning(int værdi)
+        {
+            this.Værdi = værdi;
+        }
+        static Terning()
+        {
+            rnd = new Random();
+        }
+
+
+    }
+
+    public class LudoTerning : Terning
+    {
+        public bool ErGLobus()
+        {
+            return this.Værdi == 3;
+        }
+        public bool ErStjerne()
+        {
+            return this.Værdi == 5;
+        }
+        public LudoTerning():base()
+        {
+
+        }
+        public LudoTerning(int værdi) : base(værdi)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Terninger Skriv hvad du ønsker at kalde af terninger class: terning, LudoTerning..
+            new LudoTerning();
+            new Terning();
+            new LudoTerning();
+            new Terning();
+            
+            Console.ReadKey();
+
+        }
+    }
+}
